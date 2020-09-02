0459a3b1553473db459511a5b789aa3d89058bca
wavaleros <wavaleros@gmail.com>
2020-09-02 07:46:28 -0500

    CorsHeadersFix

--- main/main/settings.py
+++ main/main/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'corsheaders',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
